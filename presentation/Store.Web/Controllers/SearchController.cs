e1f8233c0dcccd1c5f8a7c15060d00b226dee0ac
Алекс Мосиса <mosisa.alex@gmail.com>
2020-07-09 23:44:54 +0300

    Added search page

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepoository bookRepository;
+
+        public SearchController(IBookRepoository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
