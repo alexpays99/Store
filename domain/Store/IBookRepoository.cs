5f8b54e01dee5d4b39773b0a53bcf91a6c3bbfe6
Алекс Мосиса <mosisa.alex@gmail.com>
2020-07-09 21:48:08 +0300

    Added an array of book repository

---
+++ domain/Store/IBookRepoository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    interface IBookRepoository
+    {
+        Book[] GetAllByTitle(string titlePart);
+    }
+}
